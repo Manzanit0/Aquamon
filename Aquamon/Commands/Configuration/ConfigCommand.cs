9317faa538e72c95a08eebff6c192f05fad0fca2
J. G. Manzano <javier.garcia@s4g.es>
2018-06-25 23:08:51 +0200

    Added some TODOs for the aquamon config command.

---
+++ Aquamon/Commands/Configuration/ConfigCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Aquamon.Commands.Configuration
+{
+    public class ConfigCommand
+    {
+        // TODO - Create a config command which will allow the user to modify the appsettings.json values.
+        // Use the Configurator.Save() method.
+        // example 1, aquamon config LoginInfo.Username "username@domain.org"
+        // example 2, aquamon config SandboxInfo.ApexClassId "123wqe234wer23"
+        
+        // TODO - Add a --get option which will allow the user to view the appsettings.json values.
+        // example 1, aquamon config --get LoginInfo.Username => returns username@domain.org
+        
+        // TODO - if there is only one argument, interpret it as a --get.
+    }
+}
